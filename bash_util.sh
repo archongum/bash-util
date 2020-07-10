4aeeb2c973f0bfef48fc2f203e7db72e51a725a8
Archon <qq349074225@live.com>
2020-07-10 11:18:00 +0800

    add function prefix

--- bash_util.sh
+++ bash_util.sh
@@ -2,22 +2,22 @@
 
 #====================== log ======================
 
-log_info() {
+function log_info() {
   # Usage: log_info "this is the info log message"
   echo "$(date "+%Y-%m-%d %H:%M:%S") [INFO]: $@"
 }
 
-log_warnning() {
+function log_warnning() {
   # Usage: log_warnning "this is the warning log message"
   echo "$(date "+%Y-%m-%d %H:%M:%S") [WARN]: $@"
 }
 
-log_error() {
+function log_error() {
   # Usage: log_error "this is the error log message"
   echo "$(date "+%Y-%m-%d %H:%M:%S") [ERROR]: $@"
 }
 
-log_exit() {
+function log_exit() {
   # Usage: log_exit "the log message before exit"
   log_error "$@"
   exit 1
@@ -25,7 +25,7 @@ log_exit() {
 
 # ====================== date ======================
 
-date_format() {
+function date_format() {
   # Usage: date_format 20200101 [formatter (DEFAULT: %Y%m%d)]
   local d
   local formatter
@@ -36,7 +36,7 @@ date_format() {
   echo $(date -d "${d}" "+${formatter}")
 }
 
-get_date_from() {
+function get_date_from() {
   # Usage: get_date_from 20200101 [interval day (DEFAULT:0)]
   local base_date
   local interval
@@ -47,39 +47,39 @@ get_date_from() {
   echo $(date -d "${base_date} ${interval} day" "+%Y%m%d")
 }
 
-get_date() {
+function get_date() {
   # Usage: get_date [interval day (DEFAULT:0)]
   get_date_from $(date "+%Y%m%d") $1
 }
 
-get_timestamp() {
+function get_timestamp() {
   # Usage: get_timestamp
   echo $(date "+%Y-%m-%d %H:%M:%S")
 }
 
-get_timestamp_compact_sec() {
+function get_timestamp_compact_sec() {
   # Usage: get_timestamp_compact_sec
   echo $(date "+%Y%m%d%H%M%S")
 }
 
-get_timestamp_compact_milli() {
+function get_timestamp_compact_milli() {
   # Usage: get_timestamp_compact_milli
   echo $(date "+%Y%m%d%H%M%S%N") | cut -c1-17
 }
 
-get_unix_milli() {
+function get_unix_milli() {
   # Usage: get_unix_milli
   echo $(date "+%s%N") | cut -c1-13
 }
 
-get_unix_sec() {
+function get_unix_sec() {
   # Usage: get_unix_sec
   echo $(date "+%s")
 }
 
 #====================== echo ======================
 
-echo_separator() {
+function echo_separator() {
   # Usage: echo_separator #
   local result
   local char
@@ -98,7 +98,7 @@ echo_separator() {
 }
 
 #====================== confirm======================
-get_confirm() {
+function get_confirm() {
   # Usage: x=$(get_confirm "do you want to continue?")
   #        if [ "$x" = "yes" ]
   QUESTION="$1"
@@ -112,7 +112,7 @@ get_confirm() {
 
 #====================== if ======================
 #====================== if-then ======================
-if_error_then_exit() {
+function if_error_then_exit() {
   # Usage: if_error_then_exit $? "fail, and exit"
   if [ "$1" -ne 0 ]; then
     log_error "$2"
@@ -120,7 +120,7 @@ if_error_then_exit() {
   fi
 }
 
-if_empty_then_exit() {
+function if_empty_then_exit() {
   # Usage: if_empty_then_exit ${1} "param 1 required"
   if [ -z "$1" ]; then
     log_error "$2"
@@ -128,7 +128,7 @@ if_empty_then_exit() {
   fi
 }
 
-if_empty_then_return_default() {
+function if_empty_then_return_default() {
   # Usage: A=$(if_empty_return_default "${1}" 123)
   if [ -z "${1}" ]; then
     echo "${2}"
@@ -137,14 +137,14 @@ if_empty_then_return_default() {
   fi
 }
 
-if_empty_then_log_warnning() {
+function if_empty_then_log_warnning() {
   # Usage: if_empty_then_log_warnning "$1" "the param 1 is empty"
   if [ -z "${1}" ]; then
     log_warnning "${2}"
   fi
 }
 
-if_path_not_exist_then_exit() {
+function if_path_not_exist_then_exit() {
   # Usage: if_path_not_exist_then_exit "/tmp/a.txt" "/tmp/a.txt is not exists"
   if [ ! -e "$1" ]; then
     log_error "$2"
@@ -153,18 +153,18 @@ if_path_not_exist_then_exit() {
 }
 
 # action
-if_file_not_exist_then_touch() {
+function if_file_not_exist_then_touch() {
   # Usage: if_file_not_exist_then_touch "/tmp/a.txt"
   [ -e "$1" ] || touch "$1"
   return $?
 }
-if_dir_not_exist_then_mkdir() {
+function if_dir_not_exist_then_mkdir() {
   # Usage: if_dir_not_exist_then_mkdir "/tmp/abc"
   [ -d "$1" ] || mkdir -p "$1"
   return $?
 }
 
-if_file_exist_then_remove() {
+function if_file_exist_then_remove() {
   # Usage: if_file_exist_then_remove "/tmp/a.txt"
   if [ -e "$1" ]; then
     rm "$1"
@@ -172,7 +172,7 @@ if_file_exist_then_remove() {
   fi
 }
 
-if_dir_exist_then_remove() {
+function if_dir_exist_then_remove() {
   # Usage: if_dir_exist_then_remove "/tmp/abc"
   if [ -e "$1" ]; then
     rm -rf "$1"
@@ -180,14 +180,14 @@ if_dir_exist_then_remove() {
   fi
 }
 
-if_file_or_dir_exist_then_move_to() {
+function if_file_or_dir_exist_then_move_to() {
   if [ -e "$1" ]; then
     mv "$1" "$2"
     return $?
   fi
 }
 
-if_file_or_dir_exist_then_copy_to() {
+function if_file_or_dir_exist_then_copy_to() {
   if [ -e "$1" ]; then
     cp -r "$1" "$2"
     return $?
@@ -196,13 +196,13 @@ if_file_or_dir_exist_then_copy_to() {
 
 #====================== is ======================
 
-is_command_exists() {
+function is_command_exists() {
   type "$1" &>/dev/null
 }
 
 #====================== dir  ======================
 
-remkdir() {
+function remkdir() {
   # Usage: remkdir "/tmp/abc"
   if [ -e "$1" ]; then
     rm -rf "$1"
@@ -219,7 +219,7 @@ remkdir() {
 # TODO: add to gen date list
 
 #====================== tar ======================
-do_tar() {
+function do_tar() {
   # Usage: do_tar example.tar.gz example
   PKG_NAME="${1}"
   DIR="${2}"
@@ -231,64 +231,64 @@ do_tar() {
 #====================== string ======================
 # reference https://github.com/dylanaraps/pure-bash-bible
 
-string_trim() {
+function string_trim() {
   # Usage: string_trim "   example   string    "
   : "${1#"${1%%[![:space:]]*}"}"
   : "${_%"${_##*[![:space:]]}"}"
   printf '%s\n' "$_"
 }
 
-string_split() {
+function string_split() {
   # Usage: string_split "string" "delimiter"
   IFS=$'\n' read -d "" -ra arr <<<"${1//$2/$'\n'}"
   printf '%s\n' "${arr[@]}"
 }
 
-string_lstrip() {
+function string_lstrip() {
   # Usage: string_lstrip "string" "pattern"
   printf '%s\n' "${1##$2}"
 }
 
-string_rstrip() {
+function string_rstrip() {
   # Usage: string_rstrip "string" "pattern"
   printf '%s\n' "${1%%$2}"
 }
 
 # Requires bash 4+
-string_to_lower() {
+function string_to_lower() {
   # Usage: string_to_lower "string"
   printf '%s\n' "${1,,}"
 }
 
 # Requires bash 4+
-string_to_upper() {
+function string_to_upper() {
   # Usage: string_to_upper "string"
   printf '%s\n' "${1^^}"
 }
 
-string_contains() {
+function string_contains() {
   # Usage: string_contains hello he
   [[ "${1}" == *${2}* ]]
 }
 
-string_starts_with() {
+function string_starts_with() {
   # Usage: string_starts_with hello he
   [[ "${1}" == ${2}* ]]
 }
 
-string_ends_with() {
+function string_ends_with() {
   # Usage: string_ends_wit hello lo
   [[ "${1}" == *${2} ]]
 }
 
-string_regex() {
+function string_regex() {
   # Usage: string_regex "string" "regex"
   [[ $1 =~ $2 ]] && printf '%s\n' "${BASH_REMATCH[1]}"
 }
 
 #====================== array ======================
 # reference https://github.com/dylanaraps/pure-bash-bible
-array_reverse() {
+function array_reverse() {
   # Usage: array_reverse "array"
   shopt -s extdebug
   f() (printf '%s\n' "${BASH_ARGV[@]}")
@@ -296,7 +296,7 @@ array_reverse() {
   shopt -u extdebug
 }
 
-array_remove_dups() {
+function array_remove_dups() {
   # Usage: array_remove_dups "array"
   declare -A tmp_array
 
@@ -307,7 +307,7 @@ array_remove_dups() {
   printf '%s\n' "${!tmp_array[@]}"
 }
 
-array_random_element() {
+function array_random_element() {
   # Usage: array_random_element "array"
   local arr=("$@")
   printf '%s\n' "${arr[RANDOM % $#]}"
@@ -315,13 +315,13 @@ array_random_element() {
 
 #====================== program ======================
 
-run_command_in_background() {
+function run_command_in_background() {
   # Usage: run_command_in_background ./some_script.sh
   (nohup "$@" &>/dev/null &)
 }
 
 #====================== others ======================
-generate_uuid() {
+function generate_uuid() {
   # Usage: generate_uuid
   C="89ab"
 
